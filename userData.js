577430201a95e19ab2a9da8bf11240f8dd762aad
katherineclare9-design <katherineclare9@gmail.com>
2026-07-29 15:33:55 -0400

    Update userData.js

--- userData.js
+++ userData.js
@@ -1,14 +1,35 @@
-// UpLift User Data System
+let userData = JSON.parse(localStorage.getItem("upliftData")) || {
 
 
-let userData = {
+    // PROFILE
 
+    profileCreated: false,
+
+    profileName: "",
+
+    athleteType: "",
+
+    goal: "",
+
+    profileDate: "",
+
+
+
+    // SETTINGS
+
+    mode: "Regular",
+
+    arfidSupport: false,
+
+
+
+    // PROGRESS
 
     xp: 0,
 
     level: 1,
 
-    xpToNextLevel: 500,
+    xpToNextLevel: 100,
 
 
     streak: 0,
@@ -17,19 +38,14 @@ let userData = {
     workoutsCompleted: 0,
 
 
-    checklistCompleted: 0,
-
-
     completedWorkouts: [],
 
 
     unlockedBadges: [],
 
 
-    lastWorkoutDate: null,
 
-
-    // Workout Categories
+    // CATEGORIES
 
     coreWorkouts: 0,
 
@@ -37,20 +53,14 @@ let userData = {
 
     backspotWorkouts: 0,
 
-    jumpSessions: 0,
-
     flexibilitySessions: 0,
 
     lowerBodyWorkouts: 0,
 
     upperBodyWorkouts: 0,
 
+    jumpSessions: 0,
 
-    // Settings
-
-    mode: "Regular",
-
-    arfidSupport: false
 
 };
 
@@ -60,7 +70,6 @@ let userData = {
 
 function saveUserData() {
 
-
     localStorage.setItem(
 
         "upliftData",
@@ -68,77 +77,6 @@ function saveUserData() {
         JSON.stringify(userData)
 
     );
-
-
-}
-
-
-
-
-
-function loadUserData() {
-
-
-    const savedData = localStorage.getItem("upliftData");
-
-
-    if (savedData) {
-
-
-        userData = {
-
-            ...userData,
-
-            ...JSON.parse(savedData)
-
-        };
-
-
-    }
-
-
-}
-
-
-
-
-
-function addXP(amount) {
-
-
-    userData.xp += amount;
-
-
-
-    while (userData.xp >= userData.xpToNextLevel) {
-
-
-        userData.xp -= userData.xpToNextLevel;
-
-
-        userData.level += 1;
-
-
-        userData.xpToNextLevel = Math.floor(
-
-            userData.xpToNextLevel * 1.25
-
-        );
-
-
-        alert(
-
-            "🎉 Level Up! You reached Level " + userData.level
-
-        );
-
-
-    }
-
-
-
-    saveUserData();
-
 
 }
 
@@ -169,9 +107,3 @@ function resetProgress() {
 
 
 }
-
-
-
-
-
-loadUserData();
